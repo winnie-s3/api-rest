302228f104f72546d8080cdbb3e398f8e9aabe6b
“Winnie <winniestefany303@gmail.com>
2024-04-06 02:17:57 -0300

    criando api restful com node, express e mongoose

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // config inicial
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 // forma de ler JSON / midlewares (recursos executados entre as nossas requisicoes e respostas)
 app.use(
@@ -11,15 +13,26 @@ app.use(
 
 app.use(express.json())
 
-// rota inicial / endpoint
-app.get('/', (req, res) => {
+// rotas da API
+const personRoutes = require('./routes/personRoutes')
 
-  // mostrar req
+app.use('/person', personRoutes)
 
-  res.json({message: 'Oi Express!'})
-
-})
+// nkyWnEqLo0cViH35
+// mongodb+srv://winniestefany303:nkyWnEqLo0cViH35@apicluster.tbgprir.mongodb.net/bancodaapi?retryWrites=true&w=majority&appName=APICluster
 
 
 // entregar uma porta
-app.listen(3100)
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.tbgprir.mongodb.net/?retryWrites=true&w=majority&appName=APICluster`,
+  )
+  .then(() => {
+    console.log('Conectamos ao MongoDB!')
+    app.listen(3100)
+  })
+  .catch((err) => console.log(err))
